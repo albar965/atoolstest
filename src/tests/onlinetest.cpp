643c53720dc83d6ce71c0491af2bcd4417c2afaf
Alexander Barthel <albar965@mailbox.org>
2021-03-24 21:36:45 +0100

    Added tests for new VATSIM JSON feed and adapted some tests to gzip encoding changes. albar965/littlenavmap#606

--- src/tests/onlinetest.cpp
+++ src/tests/onlinetest.cpp
@@ -20,7 +20,9 @@
 #include "testutil/testutil.h"
 #include "sql/sqldatabase.h"
 #include "sql/sqlutil.h"
+#include "zip/gzip.h"
 #include "atools.h"
+#include "util/httpdownloader.h"
 #include "fs/online/onlinedatamanager.h"
 
 using atools::sql::SqlDatabase;
@@ -40,21 +42,32 @@ void OnlineTest::runtest(int argc, char *argv[])
 
 void OnlineTest::initTestCase()
 {
-  dbIvao = testutil::createDb("TESTDBIVAO", "online_test_ivao.sqlite");
   dbVatsim = testutil::createDb("TESTDBVATSIM", "online_test_vatsim.sqlite");
+  dbVatsim3 = testutil::createDb("TESTDBVATSIM3", "online_test_vatsim3.sqlite");
+  dbIvao = testutil::createDb("TESTDBIVAO", "online_test_ivao.sqlite");
   dbCustom = testutil::createDb("TESTDBCUSTOM", "online_test_custom.sqlite");
 }
 
 void OnlineTest::cleanupTestCase()
 {
-  testutil::removeDb(dbIvao, "TESTDBIVAO");
   testutil::removeDb(dbVatsim, "TESTDBVATSIM");
+  testutil::removeDb(dbVatsim3, "TESTDBVATSIM3");
+  testutil::removeDb(dbIvao, "TESTDBIVAO");
   testutil::removeDb(dbCustom, "TESTDBCUSTOM");
 }
 
 void OnlineTest::testCreateSchemaVatsim()
 {
   OnlinedataManager odm(dbVatsim, true);
+  odm.createSchema();
+  odm.initQueries();
+  QCOMPARE(odm.hasSchema(), true);
+  QCOMPARE(odm.hasData(), false);
+}
+
+void OnlineTest::testCreateSchemaVatsim3()
+{
+  OnlinedataManager odm(dbVatsim3, true);
   odm.createSchema();
   odm.initQueries();
   QCOMPARE(odm.hasSchema(), true);
@@ -88,10 +101,34 @@ void OnlineTest::testOpenStatusVatsim()
 
   for(int i = 0; i < 10; i++)
   {
-    bool gzipped;
-    QString url0 = odm.getWhazzupUrlFromStatus(gzipped);
+    bool gzipped, json;
+    QString url0 = odm.getWhazzupUrlFromStatus(gzipped, json);
     qInfo() << "url0" << url0;
     QCOMPARE(url0.startsWith("http://"), true);
+    QCOMPARE(gzipped, false);
+    QCOMPARE(json, false);
+
+    QString url1 = odm.getWhazzupVoiceUrlFromStatus();
+    qInfo() << "url1" << url1;
+    QCOMPARE(url1.startsWith("http://"), true);
+  }
+}
+
+void OnlineTest::testOpenStatusVatsim3()
+{
+  OnlinedataManager odm(dbVatsim, true);
+  odm.readFromStatus(atools::strFromFile(":/test/resources/vatsim-status-json.txt"));
+
+  QCOMPARE(odm.getMessageFromStatus(), QString("VATSIM TEST message"));
+
+  for(int i = 0; i < 10; i++)
+  {
+    bool gzipped, json;
+    QString url0 = odm.getWhazzupUrlFromStatus(gzipped, json);
+    qInfo() << "url0" << url0;
+    QCOMPARE(url0.startsWith("https://"), true);
+    QCOMPARE(gzipped, false);
+    QCOMPARE(json, true);
 
     QString url1 = odm.getWhazzupVoiceUrlFromStatus();
     qInfo() << "url1" << url1;
@@ -108,8 +145,8 @@ void OnlineTest::testOpenStatusIvao()
 
   for(int i = 0; i < 10; i++)
   {
-    bool gzipped;
-    QString url0 = odm.getWhazzupUrlFromStatus(gzipped);
+    bool gzipped, json;
+    QString url0 = odm.getWhazzupUrlFromStatus(gzipped, json);
     qInfo() << "url0" << url0;
     QCOMPARE(url0.startsWith("http://"), true);
 
@@ -130,11 +167,36 @@ void OnlineTest::testOpenWhazzupVatsim()
   QCOMPARE(odm.hasSchema(), true);
   QCOMPARE(odm.hasData(), true);
   QCOMPARE(odm.getReloadMinutesFromWhazzup(), 2);
-  QCOMPARE(odm.getAtisAllowMinutesFromWhazzup(), 5);
-  QCOMPARE(odm.getLastUpdateTimeFromWhazzup(), QDateTime(QDate(2018, 3, 22), QTime(17, 0, 14))); // 20180322170014
+  QCOMPARE(odm.getLastUpdateTimeFromWhazzup(), QDateTime(QDate(2018, 3, 22), QTime(17, 0, 14)));
+
+  QDebug dbg(qDebug());
+  SqlUtil(dbVatsim3).printTableStats(dbg, {"client", "atc", "server"}, true);
   QCOMPARE(SqlUtil(dbVatsim).rowCount("client"), 543); // 586
   QCOMPARE(SqlUtil(dbVatsim).rowCount("atc"), 65);
   QCOMPARE(SqlUtil(dbVatsim).rowCount("server"), 41);
+}
+
+void OnlineTest::testOpenWhazzupVatsim3()
+{
+  OnlinedataManager odm(dbVatsim3, true);
+  odm.initQueries();
+  odm.readFromTransceivers(atools::strFromFile(":/test/resources/transceivers-data-fmt.json"));
+  odm.readFromWhazzup(atools::strFromFile(":/test/resources/vatsim-data-fmt.json"),
+                      atools::fs::online::VATSIM_JSON3,
+                      QDateTime(QDate(2018, 3, 22), QTime(16, 0, 00)));
+  dbVatsim3->commit();
+
+  QCOMPARE(odm.hasSchema(), true);
+  QCOMPARE(odm.hasData(), true);
+  QCOMPARE(odm.getReloadMinutesFromWhazzup(), 1);
+  QCOMPARE(odm.getLastUpdateTimeFromWhazzup(), QDateTime(QDate(2021, 3, 20), QTime(13, 0, 4, 940), Qt::UTC)); // 20180322170014
+
+  QDebug dbg(qDebug());
+  SqlUtil(dbVatsim3).printTableStats(dbg, {"client", "atc", "server", "airport"}, true);
+  QCOMPARE(SqlUtil(dbVatsim3).rowCount("client"), 1470);
+  QCOMPARE(SqlUtil(dbVatsim3).rowCount("atc"), 155);
+  QCOMPARE(SqlUtil(dbVatsim3).rowCount("server"), 7);
+  QCOMPARE(SqlUtil(dbVatsim3).rowCount("airport"), 57);
 }
 
 void OnlineTest::testOpenWhazzupIvao()
@@ -147,8 +209,10 @@ void OnlineTest::testOpenWhazzupIvao()
   QCOMPARE(odm.hasSchema(), true);
   QCOMPARE(odm.hasData(), true);
   QCOMPARE(odm.getReloadMinutesFromWhazzup(), 1);
-  QCOMPARE(odm.getAtisAllowMinutesFromWhazzup(), 0);
   QCOMPARE(odm.getLastUpdateTimeFromWhazzup(), QDateTime(QDate(2018, 3, 21), QTime(15, 54, 54))); // 20180321155454
+
+  QDebug dbg(qDebug());
+  SqlUtil(dbVatsim3).printTableStats(dbg, {"client", "atc", "server"}, true);
   QCOMPARE(SqlUtil(dbIvao).rowCount("client"), 599); // 691
   QCOMPARE(SqlUtil(dbIvao).rowCount("atc"), 92);
   QCOMPARE(SqlUtil(dbIvao).rowCount("server"), 6);
@@ -165,7 +229,6 @@ void OnlineTest::testOpenServersVatsim()
   QCOMPARE(odm.hasSchema(), true);
   QCOMPARE(odm.hasData(), true);
   QCOMPARE(odm.getReloadMinutesFromWhazzup(), 2);
-  QCOMPARE(odm.getAtisAllowMinutesFromWhazzup(), 5);
   QCOMPARE(SqlUtil(dbVatsim).rowCount("server"), 41);
 }
 
@@ -180,7 +243,6 @@ void OnlineTest::testOpenServersIvao()
   QCOMPARE(odm.hasSchema(), true);
   QCOMPARE(odm.hasData(), true);
   QCOMPARE(odm.getReloadMinutesFromWhazzup(), 15);
-  QCOMPARE(odm.getAtisAllowMinutesFromWhazzup(), 0);
   QCOMPARE(SqlUtil(dbIvao).rowCount("server"), 12);
 }
 
@@ -195,8 +257,10 @@ void OnlineTest::testOpenWhazzupCustom()
   QCOMPARE(odm.hasSchema(), true);
   QCOMPARE(odm.hasData(), true);
   QCOMPARE(odm.getReloadMinutesFromWhazzup(), 1);
-  QCOMPARE(odm.getAtisAllowMinutesFromWhazzup(), 0);
   QCOMPARE(odm.getLastUpdateTimeFromWhazzup(), QDateTime(QDate(2017, 9, 26), QTime(20, 12, 32))); // 20170926201232
+
+  QDebug dbg(qDebug());
+  SqlUtil(dbVatsim3).printTableStats(dbg, {"client", "atc", "server"}, true);
   QCOMPARE(SqlUtil(dbCustom).rowCount("client"), 10);
   QCOMPARE(SqlUtil(dbCustom).rowCount("atc"), 1);
   QCOMPARE(SqlUtil(dbCustom).rowCount("server"), 1);
@@ -233,4 +297,55 @@ void OnlineTest::testDropSchemaCustom()
   QCOMPARE(odm.hasSchema(), false);
   QCOMPARE(odm.hasData(), false);
 #endif
+}
+
+void OnlineTest::testVatsimJson3Download()
+{
+  atools::util::HttpDownloader downloader(this, true);
+  downloader.setUrl("https://data.vatsim.net/v3/vatsim-data.json");
+
+  bool errorFlag = false, finished = false;
+
+  downloader.startDownload();
+
+  connect(&downloader, &atools::util::HttpDownloader::downloadFinished,
+          [&finished, &errorFlag](const QByteArray& data, QString downloadUrl) -> void
+  {
+    qDebug() << Q_FUNC_INFO << data.size() << downloadUrl;
+    errorFlag = false;
+    finished = true;
+  });
+
+  connect(&downloader, &atools::util::HttpDownloader::downloadFailed,
+          [&errorFlag, &finished](const QString& error, int errorCode, QString downloadUrl) -> void
+  {
+    qDebug() << Q_FUNC_INFO << error << errorCode << downloadUrl;
+    errorFlag = true;
+    finished = true;
+  });
+
+  testutil::waitForValue(finished, 180);
+
+  qDebug() << Q_FUNC_INFO << "errorFlag" << errorFlag << "size" << downloader.getData().size();
+
+  QVERIFY(!atools::zip::isGzipCompressed(downloader.getData()));
+  QVERIFY(QString(downloader.getData()).startsWith("{"));
+  QVERIFY(downloader.getData().size() > 100);
+  QVERIFY(!errorFlag);
+  QVERIFY(finished);
+
+  errorFlag = finished = false;
+  downloader.setAcceptEncoding("gzip");
+
+  downloader.startDownload();
+
+  testutil::waitForValue(finished, 180);
+
+  qDebug() << Q_FUNC_INFO << "errorFlag" << errorFlag << "size" << downloader.getData().size();
+
+  QVERIFY(atools::zip::isGzipCompressed(downloader.getData()));
+  QVERIFY(!QString(downloader.getData()).startsWith("{"));
+  QVERIFY(downloader.getData().size() > 100);
+  QVERIFY(!errorFlag);
+  QVERIFY(finished);
 }
