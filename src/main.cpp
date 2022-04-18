6db5a35682146da37066558c52db0e59053f4bad
Alexander Barthel <albar965@mailbox.org>
2022-04-18 13:34:25 +0200

    Version 3.7.0.develop to align with atools.

--- src/main.cpp
+++ src/main.cpp
@@ -121,13 +121,14 @@ int main(int argc, char *argv[])
   QCoreApplication::setApplicationName("atoolstest");
   QCoreApplication::setOrganizationName("ABarthel");
   QCoreApplication::setOrganizationDomain("littlenavmap.org");
-  QCoreApplication::setApplicationVersion("1.0.2"); // VERSION_NUMBER
+  QCoreApplication::setApplicationVersion("3.7.0.develop"); // VERSION_NUMBER
 
   qStdOut() << "SSL supported" << QSslSocket::supportsSsl()
             << "build library" << QSslSocket::sslLibraryBuildVersionString()
             << "library" << QSslSocket::sslLibraryVersionString() << endl;
 
   qStdOut() << "Starting tests ..." << endl;
+  qStdOut() << QCoreApplication::applicationName() << QCoreApplication::applicationVersion() << endl;
   qStdOut() << "LANG=" << QProcessEnvironment::systemEnvironment().value("LANG") << endl;
   qStdOut() << "LANGUAGE=" << QProcessEnvironment::systemEnvironment().value("LANGUAGE") << endl;
 
